f77fcf182480452e8ebf1eff5ce7a0ca52c8a485
altman-powell <powell_758258_tx@163.com>
2019-04-04 11:42:08 +0800

    Create config.js

---
+++ epgms_powell/WebRoot/template/ahiptvdemo/js/config.js
@@ -0,0 +1,9 @@
+var config_basepath = "http://117.71.25.104:81/bytuetechAPI/static_data/";
+var config_jsDetailPath= "js/data/detail/";
+var config_jsCategoryPath = "js/data/categorylist/";
+var config_jsContentPath = "js/data/programlist/";
+var config_jsChannelPath = "js/data/channellist/";
+var config_jsADPath = "js/data/ad/";
+var config_moviebasecategoryid = "1001";
+var config_seriesbasecategoryid = "1002,1005";
+var config_varietybasecategoryid = "1003,1004";
