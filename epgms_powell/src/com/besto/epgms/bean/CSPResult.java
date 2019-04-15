a0a4618041ec72bc03f09c515cb416657f4626b4
altman-powell <powell_758258_tx@163.com>
2019-04-15 15:52:46 +0800

    Create CSPResult.java

---
+++ epgms_powell/src/com/besto/epgms/bean/CSPResult.java
@@ -0,0 +1,149 @@
+package com.besto.epgms.bean;
+
+public class CSPResult  implements java.io.Serializable {
+    private int Result;
+
+
+	public int getResult() {
+		return Result;
+	}
+
+	public void setResult(int result) {
+		this.Result = Result;
+	}
+
+	private java.lang.String errorDescription;
+
+    public CSPResult() {
+    }
+
+    public CSPResult(
+           int Result,
+           java.lang.String errorDescription) {
+           this.Result = Result;
+           this.errorDescription = errorDescription;
+    }
+
+
+    /**
+     * Gets the result value for this CSPResult.
+     * 
+     * @return result
+     */
+
+
+
+    /**
+     * Sets the result value for this CSPResult.
+     * 
+     * @param result
+     */
+
+
+
+    /**
+     * Gets the errorDescription value for this CSPResult.
+     * 
+     * @return errorDescription
+     */
+    public java.lang.String getErrorDescription() {
+        return errorDescription;
+    }
+
+
+    /**
+     * Sets the errorDescription value for this CSPResult.
+     * 
+     * @param errorDescription
+     */
+    public void setErrorDescription(java.lang.String errorDescription) {
+        this.errorDescription = errorDescription;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof CSPResult)) return false;
+        CSPResult other = (CSPResult) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            this.Result == other.getResult() &&
+            ((this.errorDescription==null && other.getErrorDescription()==null) || 
+             (this.errorDescription!=null &&
+              this.errorDescription.equals(other.getErrorDescription())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        _hashCode += getResult();
+        if (getErrorDescription() != null) {
+            _hashCode += getErrorDescription().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(CSPResult.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("iptv", "CSPResult"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("result");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "Result"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "int"));
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("errorDescription");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "ErrorDescription"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://schemas.xmlsoap.org/soap/encoding/", "string"));
+        elemField.setNillable(true);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
