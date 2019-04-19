4bb0a37e8e5c8f9e0b310d80323e3c2b2e84d09a
altman-powell <powell_758258_tx@163.com>
2019-04-19 11:05:07 +0800

    Create P1099004002007002.js

---
+++ epgms_powell/WebRoot/static_data/js/data/programlist/1/1099/1099004/1099004002/1099004002007/1099004002007002/P1099004002007002.js
@@ -0,0 +1,1 @@
+ContentListCallback([{"cliackamount":null,"marklist":[],"description":"简单易懂，寓教于乐","seriesflag":"1","name":"韵律数学","fileurl":"http://117.71.25.104:85/pic/cms/program/00000001000000100000000000247885/1505727737637type1.png","charging":"0","category_id":"1099004002007002","orgairdate":"","code":"00000001000000100000000000247885","primaryid":"2189912"}])
