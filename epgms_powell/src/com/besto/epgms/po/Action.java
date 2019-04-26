223f4da7cceeccd4c9e4f12c0ec3d8bf8cb2abd7
altman-powell <powell_758258_tx@163.com>
2019-04-26 15:08:07 +0800

    Create Action.java

---
+++ epgms_powell/src/com/besto/epgms/po/Action.java
@@ -0,0 +1,70 @@
+package com.besto.epgms.po;
+
+public class Action {
+	private String id;  //主键
+	private String code;  //菜单编号
+	private String name;  //菜单名称
+	private String action;  //菜单动作
+	private String sequence;  //排序
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the action
+	 */
+	public String getAction() {
+		return action;
+	}
+	/**
+	 * @param action the action to set
+	 */
+	public void setAction(String action) {
+		this.action = action;
+	}
+	/**
+	 * @return the sequence
+	 */
+	public String getSequence() {
+		return sequence;
+	}
+	/**
+	 * @param sequence the sequence to set
+	 */
+	public void setSequence(String sequence) {
+		this.sequence = sequence;
+	}
+	
+} 
