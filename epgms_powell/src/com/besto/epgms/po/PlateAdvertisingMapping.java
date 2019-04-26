7581203b18b54d0858bfaab6716f522a3a576e6e
altman-powell <powell_758258_tx@163.com>
2019-04-26 15:08:29 +0800

    Create PlateAdvertisingMapping.java

---
+++ epgms_powell/src/com/besto/epgms/po/PlateAdvertisingMapping.java
@@ -0,0 +1,33 @@
+package com.besto.epgms.po;
+
+public class PlateAdvertisingMapping {
+	private int id;
+	private int plateid;
+	private int advertisingid;
+	private String remark;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPlateid() {
+		return plateid;
+	}
+	public void setPlateid(int plateid) {
+		this.plateid = plateid;
+	}
+	public int getAdvertisingid() {
+		return advertisingid;
+	}
+	public void setAdvertisingid(int advertisingid) {
+		this.advertisingid = advertisingid;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+}
