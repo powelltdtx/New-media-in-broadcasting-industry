d85295e1c8ef727407401b9a3a7a7ea18f9c8325
altman-powell <powell_758258_tx@163.com>
2019-04-26 15:05:27 +0800

    Create UpstreamgroupService.java

---
+++ epgms_powell/src/com/besto/epgms/manage/UpstreamgroupService.java
@@ -0,0 +1,24 @@
+package com.besto.epgms.manage;
+
+import com.besto.epgms.vo.TempletVO;
+
+
+/**
+ * 
+*Project:  < epgms>      
+* Comments:  <描述>         
+* JDK version used:<JDK1.6>
+* Author:   <powell/滕翔>
+* Create Date: <2015-12-25>  
+* Version:  <1.0>
+ */
+public interface UpstreamgroupService {
+	
+	
+	/**
+	 */
+	public String add(TempletVO vo);
+	
+	
+	
+}
