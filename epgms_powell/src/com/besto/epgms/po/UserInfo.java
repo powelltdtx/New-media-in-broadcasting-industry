f0b4fafbecc85a79d7bab29f4210a0cb1758b80a
altman-powell <powell_758258_tx@163.com>
2019-04-26 15:08:40 +0800

    Create UserInfo.java

---
+++ epgms_powell/src/com/besto/epgms/po/UserInfo.java
@@ -0,0 +1,171 @@
+package com.besto.epgms.po;
+/**
+ * 用户信息类
+ * @author <powell/滕翔>
+ *
+ */
+public class UserInfo {
+	private String userid;
+	private String username;//用户名称
+	private String password;//密码
+	private String userrole;//用户角色（简单角色标识 初期用）  弃用
+	private String phone;//手机号
+	private String email;//邮箱
+	private String status;//状态  0生效 1失效
+	private String createperson;//创建人
+	private String createtime;//注册时间
+	private String remark;//备注
+	private String userrolename;
+	
+	public UserInfo() {
+	}
+
+	/**
+	 * @return the userid
+	 */
+	public String getUserid() {
+		return userid;
+	}
+
+	/**
+	 * @param userid the userid to set
+	 */
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the userrole
+	 */
+	public String getUserrole() {
+		return userrole;
+	}
+
+	/**
+	 * @param userrole the userrole to set
+	 */
+	public void setUserrole(String userrole) {
+		this.userrole = userrole;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public String getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public String getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the createperson
+	 */
+	public String getCreateperson() {
+		return createperson;
+	}
+
+	/**
+	 * @param createperson the createperson to set
+	 */
+	public void setCreateperson(String createperson) {
+		this.createperson = createperson;
+	}
+
+	/**
+	 * @return the createtime
+	 */
+	public String getCreatetime() {
+		return createtime;
+	}
+
+	/**
+	 * @param createtime the createtime to set
+	 */
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+
+	/**
+	 * @return the remark
+	 */
+	public String getRemark() {
+		return remark;
+	}
+
+	/**
+	 * @param remark the remark to set
+	 */
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getUserrolename() {
+		return userrolename;
+	}
+
+	public void setUserrolename(String userrolename) {
+		this.userrolename = userrolename;
+	}
+
+}
